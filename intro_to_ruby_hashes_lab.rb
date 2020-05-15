7a196432ed19ca402a06d28f3ba2013b20c7d5a6
Victor Williams <victor.williams1030@gmail.com>
2020-05-15 19:51:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	  :railroads => {
+	  :railroads => {:pieces => 4
 	  }
 	}
 # we're setting each key of the railroads hash to a sybmol, and that symbol to a value!
