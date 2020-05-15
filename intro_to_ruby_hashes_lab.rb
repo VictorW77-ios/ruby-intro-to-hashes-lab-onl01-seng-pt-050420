c6b88edec48345259fee274c89ed5f3876043be6
Victor Williams <victor.williams1030@gmail.com>
2020-05-15 19:38:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,10 +15,11 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-    :railroads => {:pieces => 4, :rent_in_dollars => { 
+    :railroads => {:pieces => 4
+    }
+    :rent_in_dollars => { 
     }, 
-    {
-      :one_piece_owned => 25
+    :one_piece_owned => 25
     }
     :names => {
     }
